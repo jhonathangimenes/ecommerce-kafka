512410ce394786294a5f6d83fad7d160b1db4a04
jhonathangimenes <gimenesjhonathan@gmail.com>
2022-05-13 17:49:53 -0300

    Incluse code

---
+++ service-fraud-detector/src/main/java/FraudDetectorService.java
@@ -0,0 +1,29 @@
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+
+import java.util.Map;
+
+public class FraudDetectorService {
+    public static void main(String[] args) {
+        FraudDetectorService fraudDetectorService = new FraudDetectorService();
+        try(KafkaService service = new KafkaService<>(FraudDetectorService.class.getSimpleName(),
+                "ECOMMERCE_NEW_ORDER", fraudDetectorService::parse, Order.class, Map.of())) {
+            service.run();
+        }
+
+    }
+
+    private void parse(ConsumerRecord<String, Order> record) {
+        System.out.println(" ----------------------------------- ");
+        System.out.println("Processing new order, checking for fraud");
+        System.out.println(record.key());
+        System.out.println(record.value());
+        System.out.println(record.partition());
+        System.out.println(record.offset());
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("br.com.gimenes.Order processed");
+    }
+}
